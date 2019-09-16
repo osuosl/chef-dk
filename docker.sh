618a212fde58187b23179a4fc16e7b1e8b44fa0a
Lance Albertson <lance@osuosl.org>
2019-09-16 11:53:39 -0700

    Don't use omnibus volume by default

--- docker.sh
+++ docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -ex
-docker volume create omnibus
-docker run -v omnibus:/var/cache/omnibus -v $(pwd):/chef-dk -it --rm $1 bash
+docker run -v $(pwd):/chef-dk -it --rm $1 bash
+
+# Uncomment for testing purposes
+# docker volume create omnibus
+# docker run -v omnibus:/var/cache/omnibus -v $(pwd):/chef-dk -it --rm $1 bash
